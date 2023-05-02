3416a5f8f2dd0e4009b33cf4d1711021f4eac0fa
mdfaysalbabu <foysalfoysal313@gmail.com>
2023-05-02 07:46:57 +0530

    add express and nodemon

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const app=express();
+const cors=require("cors");
+const port=process.env.PORT ||5000;
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send("chef is running")
+})
+
+app.listen(port,()=>{
+    console.log(`chef is running port:${port}`)
+})
