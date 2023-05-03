6ad3d449e82b5c0e8d8614e96cc7a6935969d6f5
mdfaysalbabu <foysalfoysal313@gmail.com>
2023-05-03 17:09:58 +0530

    add bio

--- index.js
+++ index.js
@@ -2,23 +2,20 @@ const express=require("express");
 const app=express();
 const cors=require("cors");
 const chef=require("./data/chefdata.json")
-const viewdetails=require("./data/recipes.json")
 const port=process.env.PORT ||5000;
 app.use(cors());
 
 app.get('/',(req,res)=>{
-    res.send("chef is running")
+    res.send(chef)
 })
 
 app.get("/chef",(req,res)=>{
     res.send(chef);
 })
-app.get("/viewdetails",(req,res)=>{
-    res.send(viewdetails);
-})
+
 app.get("/viewdetails/:id",(req,res)=>{
     const id=req.params.id 
-    const detail=viewdetails.find((pd)=>pd.id===id);
+    const detail=chef.find((pd)=>pd.id===id);
     res.send(detail)
 })
 
